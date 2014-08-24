5c0d73931dc0900d62d5f1a47dd42e9a8fca0b3c
Pete Skelly <pkskelly@bellsouth.net>
2014-08-24 13:32:35 -0400

    simple html hint grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt){
 
     grunt.initConfig ({
         pkg: grunt.file.readJSON('package.json'),
+
         typescript:{
             options: {
                 module: 'commonjs'
@@ -27,7 +28,7 @@ module.exports = function(grunt){
                   expand: true,
                   cwd: './js/_output/js/',
                   src: '**/*.js',
-                  dest: './js/_output/',
+                  dest: './js/_output/'
               }],
             },
             options: {
@@ -35,23 +36,40 @@ module.exports = function(grunt){
                 compress: {
                     drop_console: true
                 },
-                beautify: true,
-            },
+                beautify: true
+            }
         },
+
         clean: {
             options: {
 
             },
             folders: ['js/_output']
         },
+
+        htmlhint : {
+            templates: {
+                options: {
+                    'tag-pair': true,
+                    'tag-self-close': true,
+                    'tagname-lowercase': true,
+                    'attr-lowercase': true,
+                    'attr-value-not-empty': true
+                },
+                src: ['./templates/**/*.html']
+            }
+        }
     });
 
     grunt.loadNpmTasks("grunt-typescript");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-htmlhint");
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
 
 
-    grunt.registerTask("default", ['clean','typescript','jshint','uglify']);
+
+    grunt.registerTask("default", ['clean','htmlhint', 'typescript','jshint','uglify', ]);
 
 };
